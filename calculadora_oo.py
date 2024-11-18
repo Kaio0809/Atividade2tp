07737eedbf8a2e7b62d8794384ca20fc786b36eb
Kaio <128155244+Kaio0809@users.noreply.github.com>
2024-11-18 17:56:17 +0000

    teste

--- calculadora_oo.py
+++ calculadora_oo.py
@@ -1,16 +1,26 @@
 class Calculadora:
     def __init__(self):
-        pass
+        self.__acumulador = 0.0
 
-    def somar(self,operando_a,operando_b):
-        return operando_a + operando_b
+    def somar(self,operando_a,operando_b = None):
+        self.__acumulador += operando_a if operando_b is None else operando_a + operando_b
+        return self.__acumulador
     
-    def subtrair(self,operando_a, operando_b):
-        return operando_a - operando_b
+    def subtrair(self,operando_a, operando_b = None):
+        self.__acumulador = operando_a - self.__acumulador if operando_b is None else operando_a - operando_b
+        return self.__acumulador
     
-    def multiplicar(self,operando_a, operando_b):
-        return operando_a*operando_b
+    def multiplicar(self,operando_a, operando_b = None):
+        self.__acumulador = operando_a*self.__acumulador if operando_b is None else operando_a*operando_b
+        return self.__acumulador
     
-    def dividir(self,operando_a,operando_b):
-        return operando_a/operando_b if operando_b != 0 else "Erro de Divisão por Zero"
-    
+    def dividir(self,operando_a,operando_b = None):
+        try:
+            if operando_b is None:
+                self.__acumulador = operando_a / self.__acumulador
+            else:
+                self.__acumulador = operando_a / operando_b
+            return self.__acumulador
+        
+        except ZeroDivisionError:
+            return "Erro de divisão por Zero!"
--- testa_calculadora_oo.py
+++ testa_calculadora_oo.py
@@ -3,10 +3,13 @@ from calculadora_oo import Calculadora
 calc = Calculadora()
 
 def teste():
+    print(calc.dividir(10))
     print(calc.somar(10,19))
+    print(calc.somar(15))
     print(calc.subtrair(15,12))
+    print(calc.subtrair(20))
     print(calc.multiplicar(20,25))
+    print(calc.multiplicar(10))
     print(calc.dividir(10,2))
-    print(calc.dividir(10,0))
 
 teste()
